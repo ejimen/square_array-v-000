150389072fcba2c064f9071fd63c0d90787cdba1
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-26 23:49:18 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  numbers = [1,2,3]
+
+square_array(numbers)
+[1,4,9]
+
+  new_numbers = [9,10,16,25]
+
+  square_array(new_numbers)
+  [81,100,256,625]
 end
